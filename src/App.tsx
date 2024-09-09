6fd8014ab06b2d8d1843b720eb91774da58b82f5
mongsam2 <songjaehyun121476@gmail.com>
2024-09-09 22:42:15 +0900

    chor: chakra setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Text fontSize={"xx-large"} color={"red.600"} >It works</Text>
+    </div>
+  );
+}
+
+export default App;
